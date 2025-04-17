7f36b703729ce9846b00c3aace26ed0c6b9f7d08
Kat Reiz <kreiz002@fiu.edu>
2025-04-17 17:26:18 -0400

    Update device-connections.py

--- device-connections.py
+++ device-connections.py
@@ -15,9 +15,16 @@ URL_DISS = f"https://thingsboard.cloud/api/v1/{ACCESS_TOKEN_DISS}/telemetry"
 ACCESS_TOKEN_DEPTH = "1xjTUFT4fdQXu6Cdy9xb"
 URL_DEPTH = f"https://thingsboard.cloud/api/v1/{ACCESS_TOKEN_DEPTH}/telemetry"
 
+ACCESS_TOKEN_TEMP = "kaxejzegx889d6y0fhhl"
+URL_TEMP = f"https://thingsboard.cloud/api/v1/{ACCESS_TOKEN_TEMP}/telemetry"
+
+ACCESS_TOKEN_SAL = "qw8tuu3KbpCv9YHwDW74"
+URL_SAL = f"https://thingsboard.cloud/api/v1/{ACCESS_TOKEN_SAL}/telemetry"
+
 
 df=pd.read_csv("BKB_WaterQualityData_2020084.csv")
-for i, row in df.iterrows():
+df_no_nan = df.dropna()
+for i, row in df_no_nan.iterrows():
     payload1={
         "pH": row["pH (standard units)"]
     }
@@ -30,8 +37,17 @@ for i, row in df.iterrows():
         "Water Depth": row["Water Depth (m)"]
     }
 
+    payload4={
+        "Water Temp": row["Water Temp (?C)"]
+    }
+
+    payload5={
+        "Salinity": row["Salinity (ppt)"]
+    }
+
     response = requests.post(URL_PH, json=payload1)
     response2 = requests.post(URL_DISS, json=payload2)
     response3 = requests.post(URL_DEPTH, json=payload3)
-    print(f"Sent row {i+1}: {payload1}, {payload2}, {payload3} | StatusL {response.status_code}, {response2.status_code}, {response3.status_code}")
+    response4 = requests.post(URL_TEMP, json=payload4)
+    response5 = requests.post(URL_SAL, json=payload5)
     time.sleep(5)
